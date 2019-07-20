c5cc6f2d11e7bd8a9322eec2ff297343e2a2ac9c
Michael Meffie <mmeffie@sinenomine.net>
2019-07-20 13:06:36 -0400

    boggle app skeleton

--- 1/boggle_app.py
+++ 1/boggle_app.py
@@ -1,48 +1,64 @@
 #!/usr/bin/python3
 
 import tkinter as tk
-import boggle_dice
 
 class BoggleApp(tk.Frame):
-    def __init__(self, root, rows=4, cols=4):
+    def __init__(self, root):
         super().__init__(root)
-        self.rows = rows
-        self.cols = cols
-        self.parent = root
-        self.parent.title('Boogle')
+
+        self.rows = 4
+        self.cols = 4
+        self.words = []
+        self.running = False
+
+        self.root = root
+        self.root.title('Boggle')
         self.grid()
         self.create_widgets()
+        self.paint_canvas()
 
     def create_widgets(self):
-        self.canvas = tk.Canvas(self, width=600, height=600, bg='green')
-        self.panel = tk.Frame(self, width=400, height=600, bg='white')
+        self.canvas = tk.Canvas(self, width=480, height=480, bg='green')
+        self.panel = self.create_panel_widgets()
+
         self.canvas.grid(row=0, column=0, padx=10, pady=10)
-        self.panel.grid(row=0, column=1, padx=10, pady=10)
+        self.panel.grid(row=0, column=1, padx=10, pady=10, sticky=tk.N)
 
-        shake = tk.Button(self.panel, text='Shake!', command=self.shake)
-        quit = tk.Button(self.panel, text='Quit', command=self.quit)
-        shake.grid(row=0, column=0)
-        quit.grid(row=0, column=1)
+    def create_panel_widgets(self):
+        self.var_word = tk.StringVar()
+        self.var_words = tk.StringVar()
+        self.var_found = tk.StringVar()
+        self.var_missed = tk.StringVar()
 
-        self.paint_canvas()
+        panel = tk.Frame(self)
+        button_action = tk.Button(panel, command=self.shake, text='Shake!')
+        self.button_action = button_action # toggles
+        button_quit = tk.Button(panel, command=self.quit, text='Quit')
+        entry_word = tk.Entry(panel, width=32, textvariable=self.var_word)
+        button_ok = tk.Button(panel, text='Ok')
+        self.root.bind('<Return>', self.add_word)
+        button_ok.bind('<Button-1>', self.add_word)
+        message_words = tk.Message(panel, width=280, textvariable=self.var_words)
+        message_found = tk.Message(panel, width=280, textvariable=self.var_found)
+        message_missed = tk.Message(panel, width=280, textvariable=self.var_missed)
 
-    def quit(self):
-        print('Bye!')
-        self.parent.destroy()
+        button_action.grid(row=0, column=0)
+        button_quit.grid(row=0, column=1)
+        entry_word.grid(row=2, column=0)
+        button_ok.grid(row=2, column=1)
+        message_words.grid(row=5)
+        message_found.grid(row=6)
+        message_missed.grid(row=7)
 
-    def shake(self):
-        print('Shake it')
-        # randomize
-        self.paint_canvas()
+        return panel
 
-    def paint_canvas(self):
-        MARGIN = 20
+    def paint_canvas(self, letters=''):
+        MARGIN = 64
         PAD = 10
         WIDTH = 80
         HEIGHT = 80
-        BGCOLOR = 'white'
+        COLOR = 'white'
         FONT = ('Times', 18)
-        dice = boggle_dice.shake()
         for row in range(self.rows):
             for col in range(self.cols):
                 y0 = MARGIN + (row * (WIDTH + PAD))
@@ -51,9 +67,54 @@ class BoggleApp(tk.Frame):
                 x1 = x0 + HEIGHT
                 y2 = y0 + (WIDTH/2)
                 x2 = x0 + (HEIGHT/2)
-                i = (row  * self.cols) + col
-                self.canvas.create_rectangle(x0, y0, x1, y1, fill=BGCOLOR)
-                self.canvas.create_text(x2, y2, font=FONT, text=dice[i])
+                i = (row  * self.rows) + col
+                try:
+                    ch = letters[i].upper()
+                except IndexError:
+                    ch = ''
+                self.canvas.create_rectangle(x0, y0, x1, y1, fill=COLOR)
+                self.canvas.create_text(x2, y2, font=FONT, text=ch)
+
+    def quit(self):
+        self.root.destroy()
+
+    def clear(self):
+        self.var_word.set('')
+        self.var_words.set('')
+        self.var_found.set('')
+        self.var_missed.set('')
+        self.words = []
+
+    def shake(self):
+        self.button_action.configure(text='Solve', command=self.solve)
+        self.clear()
+        self.letters = 'abcdefghijklmnop'
+        self.paint_canvas(self.letters)
+        self.running = True
+
+    def add_word(self, event):
+        if not self.running:
+            return
+        word = self.var_word.get().strip()
+        self.var_word.set('')
+        if len(word) > 2 and word not in self.words:
+            self.words.append(word)
+            self.var_words.set(' '.join(self.words))
+
+    def solve(self):
+        if not self.running:
+            return
+        self.button_action.configure(text='Shake!', command=self.shake)
+
+        # TODO: solve!
+
+        found_text = 'Found:\n' + 'TODO'
+        missed_text = 'Missed:\n' + 'TODO'
+
+        self.var_found.set(found_text)
+        self.var_missed.set(missed_text)
+
+        self.running = False
 
 
 def main():
--- 1/boggle_dice.py
+++ 1/boggle_dice.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 
 import random
-import string
 
-# The faces for 6 sidied boggle dice.
-# Note that 'q' implies 'qu'.
-DICE = [
-    'ednosw',
-    'aaciot',
-    'acelrs',
-    'ehinps',
-    'eefhiy',
-    'elpstu',
-    'acdemp',
-    'gilruw',
-    'egkluy',
-    'ahmors',
-    'abilty',
-    'adenvz',
-    'bfiorx',
-    'dknotu',
-    'abjmoq',
-    'egintv',
-]
+class BoggleDice:
 
-def shake(number=16):
-   """Roll the dice."""
-   shaken = []
-   dice_ = list(DICE)  # Make a copy so we can shuffle it.
-   random.shuffle(dice_)
-   for d in dice_[0:number]:
-       letter = random.choice(d)
-       shaken.append(letter)
-   return shaken
+    # Faces of the 6-sided boggle dice, 16 of them.
+    dice = [
+        'ednosw',
+        'aaciot',
+        'acelrs',
+        'ehinps',
+        'eefhiy',
+        'elpstu',
+        'acdemp',
+        'gilruw',
+        'egkluy',
+        'ahmors',
+        'abilty',
+        'adenvz',
+        'bfiorx',
+        'dknotu',
+        'abjmoq', # The letter 'q' implies the pair 'qu'.
+        'egintv',
+    ]
+
+    def shake(self):
+       letters = []
+       dice = list(self.dice) # Make a copy to be shuffled in place.
+       random.shuffle(dice)
+       for d in dice:
+           letter = random.choice(d)
+           letters.append(letter)
+       return ''.join(letters)
 
 def main():
-    results = shake()
-    print(results)
+    dice = BoggleDice()
+    letters = dice.shake()
+    print(letters)
 
 if __name__ == '__main__':
     main()
